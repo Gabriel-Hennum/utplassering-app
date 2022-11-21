41cb076f4c82c787c9138c875091226395698843
Gabriel <gabriel.hennum@gmail.com>
2022-11-21 10:29:42 +0100

    finished vid

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
+import YoutubeEmbed from "./ytVideo";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div style={{
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+      height: '100hv'
+    }}>
+        <h1>
+          pablo
+        </h1>
+        es el puma
+      </div>
+      <div style={{
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+      height: '100hv'
+    }}>
+        <YoutubeEmbed embedId="tJpSVqmG27I" />
+      </div>
     </div>
   );
 }
+
 
 export default App;
